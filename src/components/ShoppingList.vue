05297672157a97cccee37a28c8b9bdd8743fb206
TKadyear <tamarakadyear@gmail.com>
2022-01-27 14:42:35 +0100

    First computed function

--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="shopping-list">
-    <div class="header">
+    <div
+      :class="['header text-center flex my-1', editing ? ' flex-row justify-between' : 'flex-col']"
+    >
       <h1>{{ header || 'Welcome' }}</h1>
       <button v-if="editing" @click="doEdit(false)" class="btn btn-cancel">Cancel</button>
       <button v-else @click="doEdit(true)" class="btn btn-primary">Add Item</button>
     </div>
-    <div v-if="editing" class="add-item-form">
-      <input @keyup.enter="saveItem" v-model="newItem" placeholder="Add an Item" />
-      <p class="counter">{{ characterCount }}200</p>
+    <div v-if="editing" class="grid grid-colums gap-1 align-baseline">
       <label>
         <strong>High Priority</strong>
         <input type="checkbox" v-model="newItemHighPriority" />
       </label>
-      <button :disabled="newItem.length < 5" @click="saveItem" class="btn btn-primary">Save Item</button>
+      <input @keyup.enter="saveItem" v-model="newItem" placeholder="Add an Item" />
+      <p class="counter">{{ characterCount }}/200</p>
+      <button :disabled="newItem.length < 5" @click="saveItem" s class="btn btn-primary">Save Item</button>
     </div>
     <p v-if="items.length === 0">Nice job! You've bought all your items!</p>
     <ul>
@@ -55,6 +57,12 @@ export default {
       console.log(item)
       item.purchased = !item.purchased
     }
+  },
+  computed: {
+    characterCount() {
+      return this.newItem.length;
+    },
+
   }
 }
 </script>
@@ -62,14 +70,17 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #shopping-list {
-  @apply w-3/5 my-0 mt-16 mx-auto flex flex-col bg-slate-200 text-center text-gray-800 rounded;
+  @apply w-3/5 my-0 mt-16 mx-auto flex flex-col bg-slate-200 p-4 text-gray-800 rounded;
   box-shadow: 0 4px 16px 2px #00000080;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+.grid-colums {
+  grid-template-columns: 7em 1fr 2.5em 6em;
+}
 h1 {
-  @apply text-green-900;
+  @apply font-bold text-xl text-green-900;
 }
 ul {
   @apply list-none;
